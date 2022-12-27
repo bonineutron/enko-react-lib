e5752fd641cddda5ee867517c9571bad5c228e1d
Bonilla Andres <mailofbonilla@gmail.com>
2022-12-27 15:56:54 -0500

    feat :zap:: add storybook

---
+++ src/lib/atoms/button/button.stories.tsx
@@ -0,0 +1,16 @@
+import { Button, ButtonProps } from './button';
+import { Story, Meta } from '@storybook/react';
+import React from 'react';
+
+export default {
+  component: Button,
+  title: 'Atoms/Button'
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  label: 'Prueba Uno'
+} as ButtonProps;
---
+++ src/lib/atoms/button/button.tsx
@@ -0,0 +1,9 @@
+export type ButtonProps = {
+  label: string;
+};
+
+export function Button({ label }: ButtonProps): JSX.Element {
+  return <button onClick={() => console.log('test')}>{label}</button>;
+}
+
+export default Button;
